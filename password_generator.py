c59f177d8fba7d0f1727bb44e2c33b30267650ab
Sherry <6256457+sherrycayheyhey@users.noreply.github.com>
2021-05-12 00:42:18 -0700

    cleaned up code

--- password_generator.py
+++ password_generator.py
@@ -14,22 +14,16 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
-# create the letter string
-letter_string = ""
+password = ""
+
 for num in range(nr_letters):
-  letter_string += letters[random.randint(0, len(letters) - 1)]
+  password += random.choice(letters)
 
-# create the symbol string
-symbol_string = ""
 for num in range(nr_symbols):
-  symbol_string += symbols[random.randint(0, len(symbols) - 1)]
+  password += random.choice(symbols)
 
-# create the number string
-number_string = ""
 for num in range(nr_numbers):
-  number_string += numbers[random.randint(0, len(numbers) - 1)]
-
-password = letter_string + symbol_string + number_string
+  password += random.choice(numbers)
 
 print(f"original password: {password}")
 
