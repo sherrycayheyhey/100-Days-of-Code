2556c17a65d141573be6857cb1436e8a281c9a89
Sherry <6256457+sherrycayheyhey@users.noreply.github.com>
2021-05-13 23:25:05 -0700

    Create meal_roulette.py

---
+++ meal_roulette.py
@@ -0,0 +1,8 @@
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+loser = random.randint(1, len(names)-1)
+print(names[loser] + " is going to buy the meal today!")
