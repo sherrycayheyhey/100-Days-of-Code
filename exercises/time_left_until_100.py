28b26830cdc0a820bb5940c19225dcae6b367815
Sherry <6256457+sherrycayheyhey@users.noreply.github.com>
2021-08-21 09:54:38 -0700

    day 2-3

--- exercises/time_left_until_100.py
+++ exercises/time_left_until_100.py
@@ -1,6 +1,7 @@
+
 age = input("What is your current age? ")
 
-# 90 is the life expectancy, years_left will be how many more years you're expected to live
+# 110 is the life expectancy, years_left will be how many more years you're expected to live
 years_left = 100 - int(age)
 months = years_left * 12
 weeks = years_left * 52
