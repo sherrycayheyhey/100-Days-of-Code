12bb31a8a50be8335901579ffab91780e0e1c434
Sherry <6256457+sherrycayheyhey@users.noreply.github.com>
2021-08-21 09:52:12 -0700

    day 3-3

--- exercises/leap-year.py
+++ exercises/leap-year.py
@@ -1,3 +1,5 @@
+# give logic and then let the student figure out how to code
+
 # it's a leap year if that year is evenly divisible by 4 
 # **except** every year that is evenly divisible by 100 
 # **unless** the year is also evenly divisible by 400
