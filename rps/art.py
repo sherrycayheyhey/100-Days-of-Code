3c0cff9292730cb69e9075239bdd8975702a0980
Sherry <6256457+sherrycayheyhey@users.noreply.github.com>
2022-01-27 21:32:50 -0800

    Create art.py

---
+++ rps/art.py
@@ -0,0 +1,51 @@
+from termcolor import colored
+
+normal = colored('''
+                                           
+              ======                         
+           ==        ==                              
+          ==          ==                               
+          ==          ==                               
+           ==        ==                              
+              ======                                
+                                                
+''', 'white')
+
+fire = colored('''
+   )
+  ) \\
+ / ) (
+ \(_)/ 
+''', 'yellow')
+
+electric = colored('''
+
+         ,/
+       ,'/
+     ,' /
+   .'  /_____,
+ .'____    ,' 
+      /  ,'
+     / ,'
+    /,'
+   /'
+''', 'yellow')
+
+water = colored('''
+     ,
+     )\\
+    /  \\
+   '  # '
+   ',  ,'
+     `'
+''', 'blue')
+
+grass = colored('''
+   ,   
+  /|\  
+ /\| \ 
+|˙,|/ |
+\ \|,˙/
+ ˙,|,˙ 
+   |  
+''', 'green')
